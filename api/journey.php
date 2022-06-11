f8674ee53f70824de83030e69efeb00fbc7f1877
Sam Sam <58818070+samleong123@users.noreply.github.com>
2022-06-11 09:03:48 +0800

    Add journey planner and station schedule via API form

---
+++ api/journey.php
@@ -0,0 +1,76 @@
+<?php
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+
+$max_calls_limit  = 30;
+$time_period      = 30;
+$total_user_calls = 0;
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $user_ip_address = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $user_ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $user_ip_address = $_SERVER['REMOTE_ADDR'];
+}
+
+if (!$redis->exists($user_ip_address)) {
+    $redis->set($user_ip_address, 1);
+    $redis->expire($user_ip_address, $time_period);
+    $total_user_calls = 1;
+} else {
+    $redis->INCR($user_ip_address);
+    $total_user_calls = $redis->get($user_ip_address);
+    if ($total_user_calls > $max_calls_limit) {
+   
+            $json = array("Status"=>"Fail","Message"=>"Rate limit exceeded!","IP Address"=>$user_ip_address,"Total_Calls"=>$total_user_calls,"Period"=>$time_period);
+       header('Content-type: application/json');  header('Access-Control-Allow-Origin: *');
+     header('HTTP/1.1 429 Too Many Requests');
+   echo (json_encode($json));
+        exit();
+    }
+}
+
+$originid = $_GET["origincode"];
+$destinationid = $_GET["destinationcode"];
+if (empty($originid)) {
+    echo "Please provide origin station code! <br> Query string origincode not found!";
+} else {
+    if (empty($destinationid)) {
+        echo "Please provide destination station code! <br> Query string destinationcode not found!";
+    } else {
+      $time = date("H:i");
+	     $timeexplode = explode(":",$time);
+	     $time = $timeexplode["0"].$timeexplode["1"];
+    $url = "http://myrailtime.ktmb.com.my:8080/mobileApi/api/processor/journey?origin=$originid&destination=$destinationid&datetime=$time&limited=false";
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Origin: http://myrailtime.ktmb.com.my",
+   "Pragma: no-cache",
+   "Referer: http://myrailtime.ktmb.com.my/",
+   "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33",
+   "Authorization: Bearer",
+   "Content-Type: application/json",
+   'X-Forwarded-For: '.$_SERVER["HTTP_X_FORWARDED_FOR"],
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+$data = '{"clientId":"userMobile","secretKey":"s3cr3t"}';
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo $resp;
+}
+}
+?>
